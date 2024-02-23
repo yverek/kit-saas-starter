bc75e7bb10b052c8f5f452c66f9ef04758e052aa
Mauro Dattilo <mau.dattilo@gmail.com>
2024-02-23 14:18:59 +0100

    fix: move logout button to dashboard

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-  import { enhance } from "$app/forms";
-  import { Button } from "$lib/components/ui/button";
   import type { LayoutData } from "./$types";
 
   export let data: LayoutData;
 </script>
 
-<form method="post" action="/auth/logout" use:enhance>
-  <Button type="submit" variant="outline" size="sm">Logout</Button>
-</form>
+<slot />
---
+++ src/routes/(app)/dashboard/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { enhance } from "$app/forms";
+  import Button from "$components/ui/button/button.svelte";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<form method="post" action="/auth/logout" use:enhance>
+  <Button type="submit" variant="outline" size="sm">Logout</Button>
+</form>
