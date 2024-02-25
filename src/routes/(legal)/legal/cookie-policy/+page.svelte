26f4f561db43eba3db753f2cd89b6ad100acf1b9
Mauro Dattilo <mau.dattilo@gmail.com>
2024-02-25 17:25:55 +0100

    refactor: delete useless divs in cookie policy

--- src/routes/(legal)/legal/cookie-policy/+page.svelte
+++ src/routes/(legal)/legal/cookie-policy/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
   import { APP_NAME } from "$configs/general";
-  import type { PageData } from "./$types";
-
-  export let data: PageData;
 </script>
 
 <div class="py-8 lg:py-16">
@@ -10,11 +7,11 @@
   <div class="mt-4">
     This Cookie Policy explains how {APP_NAME} ("we", "us", or "our") uses cookies and similar tracking technologies on our website and platform.
   </div>
+
   <div class="mt-6 text-lg font-bold">What are Cookies?</div>
-  <div>
-    <div>Cookies are small pieces of data stored on your device (computer or mobile device) when you visit our website or use our platform.</div>
-    <div>They are commonly used to enhance your browsing experience, remember your preferences, and analyze website traffic.</div>
-  </div>
+  <div>Cookies are small pieces of data stored on your device (computer or mobile device) when you visit our website or use our platform.</div>
+  <div>They are commonly used to enhance your browsing experience, remember your preferences, and analyze website traffic.</div>
+
   <div class="mt-6 text-lg font-bold">How We Use Cookies</div>
   <div>
     We use cookies and similar tracking technologies for the following purposes:
@@ -47,26 +44,22 @@
       </li>
     </ul>
   </div>
+
   <div class="mt-6 text-lg font-bold">Third-Party Cookies</div>
-  <div>
-    <div>We may also use third-party cookies on our website and platform for the purposes outlined above.</div>
-    <div>These third-party cookies are subject to the respective privacy policies of their providers.</div>
-  </div>
+  <div>We may also use third-party cookies on our website and platform for the purposes outlined above.</div>
+  <div>These third-party cookies are subject to the respective privacy policies of their providers.</div>
+
   <div class="mt-6 text-lg font-bold">Cookie Consent</div>
   <div>
-    <div>
-      By using our website and platform, you consent to the use of cookies and similar tracking technologies as described in this Cookie Policy.
-    </div>
-    <div>You can control and manage cookies through your browser settings.</div>
-    <div>Please note that disabling cookies may affect your browsing experience and limit the functionality of our website and platform.</div>
+    By using our website and platform, you consent to the use of cookies and similar tracking technologies as described in this Cookie Policy.
   </div>
+  <div>You can control and manage cookies through your browser settings.</div>
+  <div>Please note that disabling cookies may affect your browsing experience and limit the functionality of our website and platform.</div>
+
   <div class="mt-6 text-lg font-bold">Updates to this Cookie Policy</div>
-  <div>
-    <div>We may update this Cookie Policy from time to time to reflect changes in our practices or legal requirements.</div>
-    <div>We encourage you to review this Cookie Policy periodically for any updates or changes.</div>
-  </div>
+  <div>We may update this Cookie Policy from time to time to reflect changes in our practices or legal requirements.</div>
+  <div>We encourage you to review this Cookie Policy periodically for any updates or changes.</div>
+
   <div class="mt-6 text-lg font-bold">Contact Us</div>
-  <div>
-    <div>If you have any questions or concerns about this Cookie Policy or our use of cookies, please contact us.</div>
-  </div>
+  <div>If you have any questions or concerns about this Cookie Policy or our use of cookies, please contact us.</div>
 </div>
