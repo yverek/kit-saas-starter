901099ade3908b735f7a3f75f723c3b0da5d2f3f
Mauro Dattilo <mau.dattilo@gmail.com>
2024-02-25 11:26:57 +0100

    fix: error handler now show useful information

--- src/hooks/error.handler.ts
+++ src/hooks/error.handler.ts
@@ -1,10 +1,8 @@
 import { logger } from "$lib/logger";
 import type { HandleServerError } from "@sveltejs/kit";
 
-export const handleError: HandleServerError = ({ status, error, message }) => {
-  if (status !== 404) {
-    logger.error(error);
-  }
+export const handleError: HandleServerError = ({ status, message, error }) => {
+  logger.error({ status, message, error }, "ROUTING ERROR");
 
   // do not return sensitive data here as it will be sent to the client
   return { message };
