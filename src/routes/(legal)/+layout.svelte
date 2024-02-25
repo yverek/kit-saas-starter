36d0761d733ea5a4cc9eacf64c913b59da30e5c1
Mauro Dattilo <mau.dattilo@gmail.com>
2024-02-25 17:11:00 +0100

    feat: add layouts for legal section

---
+++ src/routes/(legal)/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Footer, Header } from "$components/layout/landing-page";
+  import type { LayoutData } from "./$types";
+
+  export let data: LayoutData;
+</script>
+
+<div class="container">
+  <Header user={data.user} />
+  <slot />
+  <Footer />
+</div>
