6b216b4172a309db31e61a9b2ef803a512d0d2b7
Mauro Dattilo <mau.dattilo@gmail.com>
2024-02-25 15:41:56 +0100

    fix: theme switcher color bug

--- src/lib/components/layout/landing-page/header.svelte
+++ src/lib/components/layout/landing-page/header.svelte
@@ -12,10 +12,10 @@
   let { user } = $props<{ user: User | null }>();
 </script>
 
-<header class="border-b px-4 py-2.5">
-  <nav class="mx-auto flex max-w-screen-xl justify-between">
+<header class="border-b py-2.5">
+  <nav class="flex justify-between">
     <a href={route("/")} class="flex items-center gap-3">
-      <img src="/logo.png" class="size-8" alt={`${APP_NAME} Logo`} />
+      <img src="/logo.png" class="size-9" alt={`${APP_NAME} Logo`} />
       <span class="hidden text-xl font-bold text-black dark:text-white sm:block">{APP_NAME}</span>
     </a>
     <ul class="hidden lg:flex lg:flex-row lg:font-medium">
@@ -28,10 +28,10 @@
         </li>
       {/each}
     </ul>
-    <div class="mr-2 flex gap-2">
-      <Button on:click={toggleMode} variant="secondary" size="icon">
-        <Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <div class="flex gap-2">
+      <Button on:click={toggleMode} variant="outline" size="icon">
+        <Sun color="black" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon color="white" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span class="sr-only">Toggle theme</span>
       </Button>
       {#if user}
