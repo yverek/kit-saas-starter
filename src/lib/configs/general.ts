21e02648c99806e00effd32b384f90d745084b93
Mauro Dattilo <mau.dattilo@gmail.com>
2024-02-27 11:39:20 +0100

    feat: update sendwelcomeemail function

--- src/lib/configs/general.ts
+++ src/lib/configs/general.ts
@@ -1,4 +1,4 @@
-// import { dev } from "$app/environment";
+import { dev } from "$app/environment";
 
 export const APP_NAME = "Kit SaaS Starter";
-// export const APP_URL = dev ? "localhost:5173" : "https://kit-saas-starter.pages.dev";
+export const APP_URL = dev ? "localhost:5173" : "https://kit-saas-starter.pages.dev";
--- src/lib/server/email/send.ts
+++ src/lib/server/email/send.ts
@@ -1,10 +1,15 @@
 import WelcomeHtml from "./templates/welcome.html?raw";
 import { sendEmail } from ".";
+import { APP_NAME, APP_URL } from "$configs/general";
+import { route } from "$lib/ROUTES";
 
-export async function sendWelcomeEmail(email: string, name: string): Promise<boolean> {
-  if (!email || !name) return false;
+export async function sendWelcomeEmail(email: string, name: string, tokenId: string): Promise<boolean> {
+  if (!name || !tokenId) return false;
 
-  const body = WelcomeHtml.replace("{{name}}", name);
+  const body = WelcomeHtml.replaceAll("{{appName}}", APP_NAME)
+    .replace("{{user}}", name)
+    .replace("{{token}}", tokenId)
+    .replace("{{url}}", APP_URL + route("/auth/verify-email/[tokenId=tokenId]", { tokenId }));
 
-  return await sendEmail(email, "Welcome buddy!", body);
+  return await sendEmail(email, `Verify your ${APP_NAME} email`, body);
 }
