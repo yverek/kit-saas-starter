dc2b0ae5eebde2f2c1e97a486f9821f772bf8d19
Mauro Dattilo <mau.dattilo@gmail.com>
2024-02-27 14:12:43 +0100

    feat: add verify email route

--- src/routes/(auth)/auth/verify-email/+page.server.ts
+++ src/routes/(auth)/auth/verify-email/+page.server.ts
@@ -1,5 +1,31 @@
+import { route } from "$lib/ROUTES";
 import type { PageServerLoad } from "./$types";
+import { redirect, type Actions } from "@sveltejs/kit";
+import emailValidationFormSchema from "$validations/email-validation-form.schema";
+import { superValidate, type Infer, message } from "sveltekit-superforms/server";
+import { zod } from "sveltekit-superforms/adapters";
+import { logger } from "$lib/logger";
 
-export const load = (async () => {
-  return {};
+export const load = (async ({ locals: { user } }) => {
+  if (!user) redirect(302, route("/auth/login"));
+
+  const form = await superValidate(zod(emailValidationFormSchema));
+
+  return { form };
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+  default: async ({ request }) => {
+    const form = await superValidate<Infer<typeof emailValidationFormSchema>, FlashMessage>(request, zod(emailValidationFormSchema));
+
+    if (!form.valid) {
+      logger.debug(form, "Invalid email verification form");
+
+      return message(form, { status: "error", text: "Invalid form" });
+    }
+
+    const { token } = form.data;
+
+    redirect(302, route("/auth/verify-email/[token=token]", { token }));
+  }
+};
