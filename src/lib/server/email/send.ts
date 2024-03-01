9a16204b4b361c83521891ada50a0308a2a97676
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-01 16:34:19 +0100

    feat: add password reset email

--- src/lib/server/email/send.ts
+++ src/lib/server/email/send.ts
@@ -1,5 +1,6 @@
-import WelcomeHtml from "./templates/welcome.html?raw";
 import EmailVerificationHtml from "./templates/email-verification.html?raw";
+import PasswordResetHtml from "./templates/password-reset.html?raw";
+import WelcomeHtml from "./templates/welcome.html?raw";
 import { sendEmail } from ".";
 import { APP_NAME, APP_URL } from "$configs/general";
 import { route } from "$lib/ROUTES";
@@ -20,4 +21,12 @@ export async function sendWelcomeEmail(email: string, name: string): Promise<boo
     .replace("{{url}}", APP_URL + route("/dashboard"));
 
   return await sendEmail(email, `Welcome to ${APP_NAME}`, body);
+}
+
+export async function sendPasswordResetEmail(email: string, code: string): Promise<boolean> {
+  if (!email || !code) return false;
+
+  const body = PasswordResetHtml.replaceAll("{{appName}}", APP_NAME).replace("{{code}}", code);
+
+  return await sendEmail(email, `Reset your password for ${APP_NAME}`, body);
 }
