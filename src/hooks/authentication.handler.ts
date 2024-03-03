325cf3de7d9d711db7f90d2bb3a890d682c155a5
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-03 13:52:26 +0100

    docs: add comments in authentication handler

--- src/hooks/authentication.handler.ts
+++ src/hooks/authentication.handler.ts
@@ -7,6 +7,8 @@ export const authentication: Handle = async ({ event, resolve }) => {
   event.locals.lucia = initializeLucia(event.platform?.env.DB as D1Database);
 
   const lucia = event.locals.lucia;
+
+  // retrieve the sessionId from the browser's cookies
   const sessionId = event.cookies.get(lucia.sessionCookieName);
 
   if (!sessionId) {
@@ -17,12 +19,14 @@ export const authentication: Handle = async ({ event, resolve }) => {
 
   const { session, user } = await lucia.validateSession(sessionId);
 
-  if (session && session.fresh) {
+  // if the session is newly created (due to session expiration extension), generate a new session cookie
+  if (session?.fresh) {
     const { name, value, attributes } = lucia.createSessionCookie(session.id);
 
     event.cookies.set(name, value, { ...attributes, path: "/" });
   }
 
+  // if the session is invalid, generate a blank session cookie to remove the existing session cookie from the browser
   if (!session) {
     const { name, value, attributes } = lucia.createBlankSessionCookie();
 
