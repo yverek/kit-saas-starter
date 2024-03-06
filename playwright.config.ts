c1d40d13b39cf8ff165bf1abd7601db9e6a85a8f
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-06 10:01:46 +0100

    tests: fix playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,8 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: "pnpm run build && pnpm run preview",
-    port: 4173
+    command: "pnpm run preview",
+    port: 8788
   },
   testDir: "tests",
   testMatch: /(.+\.)?(test|spec)\.[jt]s/
