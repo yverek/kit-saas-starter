de1cf6cd54eaa8c39590c850f56a72ca39215be1
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-09 15:57:23 +0100

    feat: upgrade migrations

--- migrations/0000_lame_whizzer.sql
+++ migrations/0000_lame_whizzer.sql
@@ -7,13 +7,6 @@ CREATE TABLE `oauth_accounts` (
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE TABLE `password_reset_tokens` (
-	`token` text(15) PRIMARY KEY NOT NULL,
-	`expires_at` integer NOT NULL,
-	`user_id` text NOT NULL,
-	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
 CREATE TABLE `sessions` (
 	`id` text(40) PRIMARY KEY NOT NULL,
 	`expires_at` integer NOT NULL,
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,5 +1,4 @@
 export * from "./oauth-accounts/schema";
-export * from "./password-reset-tokens/schema";
 export * from "./sessions/schema";
 export * from "./tokens/schema";
 export * from "./users/schema";
