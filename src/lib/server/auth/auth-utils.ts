baa1c77a67bb2d1523efec635bc728f482ee5d5f
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-09 14:37:18 +0100

    fix: remove email verification tokens table and utility functions

--- src/lib/server/auth/auth-utils.ts
+++ src/lib/server/auth/auth-utils.ts
@@ -1,12 +1,6 @@
 import { Lucia, generateId } from "lucia";
 import { TimeSpan, createDate, isWithinExpirationDate } from "oslo";
-import { generateRandomString, alphabet } from "oslo/crypto";
-import {
-  createEmailVerificationToken,
-  deleteAllEmailVerificationTokensByUserId,
-  deleteEmailVerificationToken,
-  getEmailVerificationTokenByUserId
-} from "../db/email-verification-tokens";
+
 import type { Database } from "../db";
 import { SESSION_ID_LEN, TOKEN_EXPIRATION_TIME, TOKEN_LEN } from "$configs/fields-length";
 import {
@@ -24,40 +18,6 @@ import {
 } from "../db/email-change-tokens";
 import type { Cookies } from "@sveltejs/kit";
 import { createToken, deleteAllTokensByUserId, type DbToken, type TOKEN_TYPE, getTokenByUserId, deleteToken } from "../db/tokens";
-
-export async function generateEmailVerificationToken(db: Database, userId: string, email: string): Promise<string> {
-  await deleteAllEmailVerificationTokensByUserId(db, userId);
-
-  const token = generateRandomString(TOKEN_LEN, alphabet("0-9", "a-z", "A-Z"));
-  const expiresAt = createDate(new TimeSpan(TOKEN_EXPIRATION_TIME, "m"));
-
-  await createEmailVerificationToken(db, { userId, email, token, expiresAt });
-
-  return token;
-}
-
-export async function verifyEmailVerificationToken(db: Database, userId: string, email: string, token: string): Promise<boolean> {
-  const tokenFromDatabase = await getEmailVerificationTokenByUserId(db, userId);
-  if (!tokenFromDatabase || tokenFromDatabase.token !== token) {
-    return false;
-  }
-
-  const res = await deleteEmailVerificationToken(db, token);
-  if (!res) {
-    return false;
-  }
-
-  const isExpired = !isWithinExpirationDate(tokenFromDatabase.expiresAt);
-  // TODO this is unnecessary, delete email field from the table and all the logic
-  const isDifferentUser = tokenFromDatabase.email !== email;
-
-  if (isExpired || isDifferentUser) {
-    return false;
-  }
-
-  return true;
-}
-
 export async function generatePasswordResetToken(db: Database, userId: string): Promise<string | undefined> {
   await deleteAllPasswordResetTokensByUserId(db, userId);
   const token = generateId(TOKEN_LEN);
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,5 +1,4 @@
 export * from "./email-change-tokens/schema";
-export * from "./email-verification-tokens/schema";
 export * from "./oauth-accounts/schema";
 export * from "./password-reset-tokens/schema";
 export * from "./sessions/schema";
