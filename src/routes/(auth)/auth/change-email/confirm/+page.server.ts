c139cab4e367bc15072ecd863f140df5960908ea
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-09 15:30:57 +0100

    feat: change email change token auth utilities

--- src/routes/(auth)/auth/change-email/confirm/+page.server.ts
+++ src/routes/(auth)/auth/change-email/confirm/+page.server.ts
@@ -1,13 +1,15 @@
 import { route } from "$lib/ROUTES";
 import type { PageServerLoad } from "./$types";
 import type { Actions } from "@sveltejs/kit";
-import { changeEmailFormSchemaSecondStep, type ChangeEmailFormSchemaSecondStep } from "$validations/auth";
+import { changeEmailFormSchemaFirstStep, changeEmailFormSchemaSecondStep, type ChangeEmailFormSchemaSecondStep } from "$validations/auth";
 import { superValidate, message } from "sveltekit-superforms/server";
 import { zod } from "sveltekit-superforms/adapters";
 import { logger } from "$lib/logger";
-import { verifyChangeEmailToken } from "$lib/server/auth/auth-utils";
 import { updateUserById } from "$lib/server/db/users";
 import { redirect } from "sveltekit-flash-message/server";
+import { verifyToken } from "$lib/server/auth/auth-utils";
+import { TOKEN_TYPE } from "$lib/server/db/tokens";
+import { dev } from "$app/environment";
 
 export const load = (async ({ locals: { user } }) => {
   if (!user) redirect(302, route("/auth/login"));
@@ -29,18 +31,36 @@ export const actions: Actions = {
       return message(form, { status: "error", text: "Invalid form" });
     }
 
+    let newEmail = "";
     const { token } = form.data;
     const { id: userId } = user;
+    const newEmailFromCookies = cookies.get("email_change");
 
-    const tokenFromDatabase = await verifyChangeEmailToken(db, userId, token);
+    const parsedNewEmail = changeEmailFormSchemaFirstStep.safeParse({ email: newEmailFromCookies });
+    if (!parsedNewEmail.success) {
+      message(form, { status: "error", text: "Invalid new email" }, { status: 401 });
+    } else {
+      newEmail = parsedNewEmail.data.email;
+    }
+
+    // TODO export this name into constant
+    cookies.delete("email_change", {
+      path: route("/auth/change-email/confirm"),
+      secure: !dev,
+      httpOnly: true,
+      maxAge: 60 * 10, // TODO should we export into a constant?
+      sameSite: "lax"
+    });
+
+    const tokenFromDatabase = await verifyToken(db, userId, token, TOKEN_TYPE.EMAIL_CHANGE);
     if (!tokenFromDatabase) {
       return message(form, { status: "error", text: "Invalid token" }, { status: 500 });
     }
-    const { email: newEmail } = tokenFromDatabase;
+
     await lucia.invalidateUserSessions(userId);
 
-    const res = await updateUserById(db, userId, { email: newEmail });
-    if (!res) {
+    const updatedUser = await updateUserById(db, userId, { email: newEmail });
+    if (!updatedUser) {
       return message(form, { status: "error", text: "User not found" }, { status: 404 });
     }
 
