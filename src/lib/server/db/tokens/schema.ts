fb58e17bb20f1ea8f5f894bea862261ca8b93493
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-09 12:22:58 +0100

    feat: add type field with enum to tokens table

--- src/lib/server/db/tokens/schema.ts
+++ src/lib/server/db/tokens/schema.ts
@@ -2,9 +2,16 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 import { users } from "../schema";
 import { TOKEN_LEN } from "../../../configs/fields-length";
 
+enum TOKEN_TYPE {
+  EMAIL_CHANGE = "email_change",
+  EMAIL_VERIFICATION = "email_verification",
+  PASSWORD_RESET = "password_reset"
+}
+
 export const tokens = sqliteTable("tokens", {
   token: text("token", { length: TOKEN_LEN }).primaryKey(),
   expiresAt: integer("expires_at", { mode: "timestamp_ms" }).notNull(),
+  type: text("type", { enum: [TOKEN_TYPE.EMAIL_CHANGE, TOKEN_TYPE.EMAIL_VERIFICATION, TOKEN_TYPE.PASSWORD_RESET] }),
   userId: text("user_id")
     .notNull()
     .references(() => users.id, { onDelete: "cascade" })
