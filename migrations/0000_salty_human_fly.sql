df00d875c3c228d0b244fbaafea8163ceaf0ccde
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-09 14:36:02 +0100

    feat: upgrade migrations

--- migrations/0000_salty_human_fly.sql
+++ migrations/0000_salty_human_fly.sql
@@ -37,6 +37,14 @@ CREATE TABLE `sessions` (
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
+CREATE TABLE `tokens` (
+	`token` text(15) PRIMARY KEY NOT NULL,
+	`expires_at` integer NOT NULL,
+	`type` text,
+	`user_id` text NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
 CREATE TABLE `users` (
 	`id` text(15) PRIMARY KEY NOT NULL,
 	`name` text,
--- migrations/0001_odd_kinsey_walden.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `tokens` (
-	`token` text(15) PRIMARY KEY NOT NULL,
-	`expires_at` integer NOT NULL,
-	`type` text,
-	`user_id` text NOT NULL,
-	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
-);
