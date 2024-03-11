29e87eb6730df18f002625d5fcec02c99064b00d
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-11 11:15:16 +0100

    feat: add turnstile svelte component

---
+++ src/lib/components/layout/Turnstile.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { Turnstile } from "svelte-turnstile";
+  import { PUBLIC_CLOUDFLARE_TURNSTILE_KEY } from "$env/static/public";
+
+  const formsField = "turnstileToken";
+  const siteKey = PUBLIC_CLOUDFLARE_TURNSTILE_KEY;
+</script>
+
+<Turnstile {siteKey} {formsField} />
