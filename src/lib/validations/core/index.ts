452c1b6aca2a567a63ea717d1fe0c26e284fd701
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-11 11:11:28 +0100

    feat: add turnstile token core field for validations

--- src/lib/validations/core/index.ts
+++ src/lib/validations/core/index.ts
@@ -3,4 +3,5 @@ export * from "./name.field";
 export * from "./password.field";
 export * from "./password-confirm.field";
 export * from "./token.field";
+export * from "./turnstile-token.field";
 export * from "./user-id.field";
