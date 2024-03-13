30369999fcff33081f493fd4627f0e3dc65515a4
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-13 14:59:59 +0100

    fix: update authorization handler

--- src/hooks/authorization.handler.ts
+++ src/hooks/authorization.handler.ts
@@ -1,6 +1,8 @@
+import { FLASH_MESSAGE_STATUS } from "$configs/general";
 import { route } from "$lib/ROUTES";
 import { logger } from "$lib/logger";
-import { redirect, type Handle } from "@sveltejs/kit";
+import { error, type Handle } from "@sveltejs/kit";
+import { redirect } from "sveltekit-flash-message/server";
 
 export const authorization: Handle = async ({ event, resolve }) => {
   const {
@@ -18,24 +20,27 @@ export const authorization: Handle = async ({ event, resolve }) => {
   const isProtectedRoute = isUserRoute || isAdminRoute;
 
   // if user is trying to access a protected route and it's not verified
-  if (isProtectedRoute && !isVerified) {
+  if (isProtectedRoute && isAuthenticated && !isVerified) {
     logger.debug(`Redirect to ${route("/auth/verify-email")} route because user is not verified`);
 
-    redirect(302, route("/auth/verify-email"));
+    redirect(303, route("/auth/verify-email"));
   }
 
   // if user is trying to access admin protected route and it's not an admin
-  if (isAdminRoute && !isAdmin) {
-    logger.debug(`Redirect to ${route("/")} route because user is not admin`);
+  if (isAdminRoute && isAuthenticated && !isAdmin) {
+    logger.debug(`Throwing 404 because someone is trying to access admin section and it is not admin`);
 
-    redirect(302, route("/"));
+    error(404);
   }
 
   // if user is trying to access an user protected route and it's not authenticated
   if (isUserRoute && !isAuthenticated) {
-    logger.debug(`Redirect to ${route("/")} route because user is not authenticated`);
+    const redirectTo = event.url.pathname;
+    const flashMessage = { status: FLASH_MESSAGE_STATUS.SUCCESS, text: "Please login first" };
 
-    redirect(302, route("/"));
+    logger.debug(`Redirect to ${route("/auth/login", { redirectTo })} route because user is not authenticated`);
+
+    redirect(route("/auth/login", { redirectTo }), flashMessage, event.cookies);
   }
 
   return resolve(event);
