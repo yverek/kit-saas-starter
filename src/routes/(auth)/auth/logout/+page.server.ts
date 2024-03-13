834a6cf1fd63764a14cb2fb44391d7450a2f2232
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-13 12:34:11 +0100

    feat: add guard for logout route

--- src/routes/(auth)/auth/logout/+page.server.ts
+++ src/routes/(auth)/auth/logout/+page.server.ts
@@ -1,16 +1,17 @@
 import { route } from "$lib/ROUTES";
 import { destroySession } from "$lib/server/auth/auth-utils";
+import { isUserAuthenticated } from "$lib/server/security";
 import type { Actions } from "./$types";
 import { redirect } from "@sveltejs/kit";
 
 export const actions: Actions = {
-  default: async ({ locals: { lucia, session }, cookies }) => {
-    if (!session) {
-      redirect(302, route("/"));
-    }
+  default: async ({ locals, cookies }) => {
+    isUserAuthenticated(locals);
 
-    await lucia.invalidateSession(session.id);
-    destroySession(lucia, cookies);
+    // ! user is defined here because of "isUserAuthenticated"
+    // TODO how can we remove that "!"?
+    await locals.lucia.invalidateSession(locals.session!.id);
+    destroySession(locals.lucia, cookies);
 
     redirect(302, route("/"));
   }
