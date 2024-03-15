6af176a998c22c81aeb3e9c26898f32982299d1d
Mauro Dattilo <mau.dattilo@gmail.com>
2024-03-15 09:41:56 +0100

    refactor: move admin route inside app route

--- src/routes/(admin)/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  let { children } = $props();
-</script>
-
-<section class="m-16">
-  <div class="mx-auto flex flex-col items-center justify-center">
-    {@render children()}
-  </div>
-</section>
